dbda6b5d2136ca9cdf22f244476df4a92a4bd015
Julio Realpe <jrealpe94@gmail.com>
2017-05-31 15:59:14 -0500

    [ADD] models and admin (backend)

--- backend/admin.py
+++ backend/admin.py
@@ -1,3 +1,148 @@
 from django.contrib import admin
+from django.contrib.auth import admin as auth_admin
+from django.contrib.auth.models import User, Group
 
-# Register your models here.
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.forms import AdminPasswordChangeForm
+from .models import *
+
+admin.site.unregister(Group)
+admin.site.unregister(User)
+
+class CategoryFilter(admin.SimpleListFilter):
+    title = _('Category')
+    parameter_name = 'category'
+
+    def lookups(self, request, model_admin):
+        categories = set([obj.category for obj in model_admin.model.objects.all()])
+        return [(c.id, c.name) for c in categories]
+
+    def queryset(self, request, queryset):
+        if self.value():
+            return queryset.filter(category__id__exact=self.value())
+        else:
+            return queryset
+
+
+class ClientFilter(admin.SimpleListFilter):
+    title = _('Client')
+    parameter_name = 'client'
+
+    def lookups(self, request, model_admin):
+        clients = set([obj.client for obj in model_admin.model.objects.all()])
+        return [(c.id, c.email) for c in clients]
+
+    def queryset(self, request, queryset):
+        if self.value():
+            return queryset.filter(client__id__exact=self.value())
+        else:
+            return queryset
+
+
+class ProviderFilter(admin.SimpleListFilter):
+    title = _('Provider')
+    parameter_name = 'provider'
+
+    def lookups(self, request, model_admin):
+        providers = set([e.provider for e in model_admin.model.objects.all()])
+        return [(e.id, e.email) for e in providers]
+
+    def queryset(self, request, queryset):
+        if self.value():
+            return queryset.filter(provider__id__exact=self.value())
+        else:
+            return queryset
+
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+    verbose_name_plural = 'Profile'
+    fk_name = 'user'
+
+
+class ProviderServiceInline(admin.StackedInline):
+    model = ProviderService
+
+
+@admin.register(User)
+class UserAdmin(auth_admin.UserAdmin):
+    inlines = (ProfileInline, ProviderServiceInline)
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2')}
+        ),
+    )
+    fieldsets = (
+        (None, {'fields': ('email', 'password',)}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name',
+                                        )}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff',
+                                        'is_superuser')}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+    list_display = ('email', 'first_name', 'last_name', 'role', 'is_staff')
+    list_filter = ('is_staff', 'is_superuser', 'is_active', 'profile__role')
+    search_fields = ('email', 'first_name', 'last_name', 'profile__role')
+    exclude_fields = ('groups', 'user_permissions')
+    ordering = ('email',)
+
+    def phone(self, instance):
+        return instance.profile.phone
+    phone.short_description = 'Teléfono'
+
+    def role(self, instance):
+        profile_role = dict(Profile.ROLE_CHOICES).get(instance.profile.role)
+        return profile_role if profile_role else 'Administrador'
+    role.short_description = 'Rol'
+
+    def get_inline_instances(self, request, obj=None):
+        if not obj:
+            return list()
+        return super(UserAdmin, self).get_inline_instances(request, obj)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'date_created', 'is_active')
+    list_filter = ('name', 'is_active')
+
+
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'date_created', 'is_active')
+    list_filter = (CategoryFilter, 'name', 'is_active')
+
+
+@admin.register(Location)
+class LocationAdmin(admin.ModelAdmin):
+    list_display = ('provider', 'latitude', 'longitude', 'date_created')
+    list_filter = (ProviderFilter,)
+
+    def get_form(self, request, obj=None, **kwargs):
+        form = super(LocationAdmin, self).get_form(request, obj, **kwargs)
+        form.base_fields['provider'].queryset = User.objects.filter(profile__role=2)
+        return form
+
+@admin.register(Reservation)
+class ReservationAdmin(admin.ModelAdmin):
+    list_display = ('client', 'provider', 'time', 'date', 'status')
+    list_filter = (ClientFilter, ProviderFilter, 'status')
+
+    def get_form(self, request, obj=None, **kwargs):
+        form = super(ReservationAdmin, self).get_form(request, obj, **kwargs)
+        form.base_fields['client'].queryset = User.objects.filter(profile__role=1)
+        form.base_fields['provider'].queryset = User.objects.filter(profile__role=2)
+        return form
+
+
+@admin.register(Evaluation)
+class EvaluationAdmin(admin.ModelAdmin):
+    list_display = ('provider', 'score', 'date_created')
+    list_filter = (ProviderFilter,)
+
+    def get_form(self, request, obj=None, **kwargs):
+        form = super(EvaluationAdmin, self).get_form(request, obj, **kwargs)
+        form.base_fields['provider'].queryset = User.objects.filter(profile__role=2)
+        return form
--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,203 @@
 from django.db import models
+from django.db.models.signals import post_save, pre_save
+from django.dispatch import receiver
+from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.utils.translation import gettext_lazy as _
+from django.utils import timezone
 
-# Create your models here.
+from rest_framework.authtoken.models import Token
+from phonenumber_field.modelfields import PhoneNumberField
+
+
+class Profile(models.Model):
+    ROLE_CHOICES = (
+        (1, 'Cliente'),
+        (2, 'Proveedor'),
+    )
+    user = models.OneToOneField(User)
+    phone = PhoneNumberField(
+        _('Phone'),
+        blank=True,
+    )
+    role = models.PositiveSmallIntegerField(
+        _('Role'),
+        choices=ROLE_CHOICES,
+        default=1,
+        blank=True,
+        null=True,
+    )
+
+    def get_token(self):
+        return self.user.auth_token.key
+    token = property(get_token)
+
+    class Meta:
+        default_related_name = 'profile'
+        verbose_name = _('Profile')
+        verbose_name_plural = _('Profile')
+
+    def __str__(self):
+        return self.user.get_full_name()
+
+
+class BaseModel(models.Model):
+    name = models.CharField(
+        _('Name'),
+        max_length=255,
+    )
+    date_created = models.DateTimeField(
+        _('date created'),
+        auto_now=True,
+    )
+    is_active = models.BooleanField(
+        _('Active'),
+        default=True,
+    )
+
+    class Meta:
+        abstract = True
+
+
+class Category(BaseModel):
+    class Meta:
+        default_related_name = 'categories'
+        verbose_name = _('Category')
+        verbose_name_plural = _('Categories')
+
+    def __str__(self):
+        return self.name
+
+
+class Service(BaseModel):
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+        verbose_name=_('Category'),
+    )
+    image = models.ImageField(
+        _('Image'),
+        upload_to='services',
+        blank = True,
+        null = True)
+
+    class Meta:
+        default_related_name = 'services'
+        verbose_name = _('Service')
+        verbose_name_plural = _('Services')
+
+    def __str__(self):
+        return self.name
+
+
+class ProviderService(models.Model):
+    provider = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name=_('Provider'),
+    )
+    service = models.ForeignKey(
+        Service,
+        on_delete=models.CASCADE,
+        verbose_name=_('Service'),
+    )
+
+    class Meta:
+        default_related_name = 'provider_services'
+        verbose_name = _('Provider Service')
+        verbose_name_plural = _('Provider Services')
+
+
+class Location(models.Model):
+    provider = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name=_('Provider'),
+    )
+    longitude = models.FloatField(_('Longitude'))
+    latitude = models.FloatField(_('Latitude'))
+    date_created = models.DateTimeField(
+        _('date created'),
+        auto_now=True,
+    )
+
+    class Meta:
+        default_related_name = 'locations'
+        verbose_name = _('Locations')
+        verbose_name_plural = _('Location')
+
+
+class Reservation(models.Model):
+    STATUS_CHOICES = (
+        (1,'Asignada'),
+        (2,'Cancelada'),
+        (3,'Pendiente'),
+        (4,'No Realizada'),
+        (5,'Realizada'),
+    )
+    client = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name=_('Client'),
+        related_name='client_reservations',
+    )
+    provider = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name=_('Provider'),
+        related_name='provider_reservations',
+        blank=True,
+        null=True,
+    )
+    time = models.TimeField(_('Time'))
+    date = models.DateField(_('Date'))
+    status = models.PositiveIntegerField(
+        _('Status'),
+        choices=STATUS_CHOICES,
+        default=3,
+        blank=True,
+    )
+
+    class Meta:
+        verbose_name = _('Reservation')
+        verbose_name_plural = _('Reservations')
+
+
+class Evaluation(models.Model):
+    provider = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name=_('Provider'),
+    )
+    score = models.PositiveIntegerField(
+        _('Score'),
+        validators=[MinValueValidator(0), MaxValueValidator(5)],
+    )
+    date_created = models.DateTimeField(
+        _('date created'),
+        auto_now=True,
+    )
+
+    class Meta:
+        default_related_name = 'evaluations'
+        verbose_name = _('Evaluation')
+        verbose_name_plural = _('Evaluations')
+
+
+@receiver(pre_save, sender=User)
+def pre_user_save(sender, instance, **kwargs):
+    if not instance.id:
+        instance.username = instance.email
+
+
+@receiver(post_save, sender=User)
+def post_user_save(sender, instance, created=False, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+    instance.profile.save()
+
+
+@receiver(post_save, sender=User)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
